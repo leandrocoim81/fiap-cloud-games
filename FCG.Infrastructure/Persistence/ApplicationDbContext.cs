c092662d0f1238b57bd09cbd93a122c734f94587
Leandro Coimbra <leandro.coimbra@gmail.com>
2025-12-27 18:10:09 -0300

    chore: Configurar ApplicationDbContext e atualizar arquivos impactados pela correção de Role

---
+++ FCG.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using FCG.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Infrastructure.Persistence
+{
+    public class ApplicationDbContext: DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _configuration = configuration;
+        }
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(_configuration.GetConnectionString("ConnectionString"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
---
+++ FCG.Infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using FCG.Application.Abstractions.Persistence;
+using FCG.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Infrastructure.Persistence
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<Guid> Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
